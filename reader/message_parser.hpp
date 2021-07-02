25828b225825c2fb19cc92e818b26f398dd36a9d
LvalueReference <lvalue.github@gmail.com>
2021-07-02 20:22:36 +0300

    Update message_parser

--- reader/message_parser.hpp
+++ reader/message_parser.hpp
@@ -13,11 +13,14 @@ namespace reader{
         simdjson::dom::element operator= (const simdjson::dom::element& json);
 
         bool has_reply();
+        bool has_fwd();
+        bool has_fwd(const simdjson::dom::element& json);
         bool has_attachments();
         bool has_attachments(const simdjson::dom::element& json);
 
-
         simdjson::dom::element get_reply();
+        simdjson::dom::array get_fwd();
+        simdjson::dom::array get_fwd(const simdjson::dom::element& json);
         simdjson::dom::array get_attachments();
         simdjson::dom::array get_attachments(const simdjson::dom::element& json);
 
@@ -26,6 +29,9 @@ namespace reader{
         std::string get_attachment_type();
         std::string get_attachment_type(const simdjson::dom::element& json);
         std::string get_reply_attachments_types();
+        std::string get_fwd_attachments_types(const simdjson::dom::element& fwd);
+        std::string get_fwd_text(const simdjson::dom::element& fwd);
+        std::string get_fwd_from(const simdjson::dom::element& fwd);
     };
 }
 
