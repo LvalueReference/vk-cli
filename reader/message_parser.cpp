0ea0fb81f2fdaef9a9267bbf28b3ceb0ef9aa4c2
LvalueReference <lvalue.github@gmail.com>
2021-07-02 20:22:36 +0300

    Update message_parser

--- reader/message_parser.cpp
+++ reader/message_parser.cpp
@@ -9,6 +9,14 @@ bool reader::message_parser::has_reply() {
     return _json["object"]["message"]["reply_message"].is_object();
 }
 
+bool reader::message_parser::has_fwd() {
+    return _json["object"]["message"]["fwd_messages"].get_array().size() != 0;
+}
+
+bool reader::message_parser::has_fwd(const simdjson::dom::element &json) {
+    return json["fwd_messages"].is_array();
+}
+
 bool reader::message_parser::has_attachments() {
     return _json["object"]["message"]["attachments"].get_array().size() != 0;
 }
@@ -22,6 +30,10 @@ simdjson::dom::element reader::message_parser::get_reply() {
     return _json["object"]["message"]["reply_message"];
 }
 
+simdjson::dom::array reader::message_parser::get_fwd() {
+    return _json["object"]["message"]["fwd_messages"].get_array();
+}
+
 simdjson::dom::array reader::message_parser::get_attachments() {
     return _json["object"]["message"]["attachments"].get_array();
 }
@@ -29,7 +41,6 @@ simdjson::dom::array reader::message_parser::get_attachments() {
 simdjson::dom::array reader::message_parser::get_attachments(const simdjson::dom::element &json) {
     return json["attachments"].get_array();
 }
-
 
 std::string reader::message_parser::get_reply_text() {
     return std::string(get_reply()["text"]);
@@ -45,7 +56,7 @@ std::string reader::message_parser::get_attachment_type() {
     return std::string(get_attachments().at(0)["type"]);
 }
 
-std::string reader::message_parser::get_attachment_type(const simdjson::dom::element &json) {
+std::string reader::message_parser::get_attachment_type(const simdjson::dom::element& json) {
     return std::string(json["type"]);
 }
 
@@ -58,4 +69,25 @@ std::string reader::message_parser::get_reply_attachments_types() {
         res.insert(0, '{' + get_attachment_type(att) + "} ");
 
     return res;
+}
+
+std::string reader::message_parser::get_fwd_attachments_types(const simdjson::dom::element& fwd) {
+    if (!has_attachments(fwd)) return "";
+
+    std::string res;
+
+    for (const auto& att : get_attachments(fwd))
+        res.insert(0, '{' + get_attachment_type(att) + "} ");
+
+    return res;
+}
+
+std::string reader::message_parser::get_fwd_text(const simdjson::dom::element& fwd) {
+    return std::string(fwd["text"]);
+}
+
+std::string reader::message_parser::get_fwd_from(const simdjson::dom::element& fwd) {
+    int from_id = fwd["from_id"].get_int64();
+
+    return (from_id > 0 ? _api.user_get(from_id) : _api.group_get(from_id));
 }
