dc0b7313e88a58578c9a5a871884255b9b336fdb
LvalueReference <lvalue.github@gmail.com>
2021-07-03 18:55:24 +0300

    Update code styling

--- reader/reader.cpp
+++ reader/reader.cpp
@@ -1,6 +1,7 @@
 #include "reader.hpp"
+#include <fmt/format.h>
 
-[[noreturn]] void reader::chat_reader::run() {
+[[noreturn]] void reader::chat_reader::run(){
     std::cout << "[ STARTED ]" << std::endl;
 
     while (true){
@@ -10,8 +11,11 @@
             for (const auto& updates : _json["updates"].get_array()){
                 _msg = updates;
 
-                std::cout << "┌[\033[32m" + _msg.from() << "\033[0m] (" << _msg.current_time()  << ")\n"
-                             "└[message]> \033[35m" << _msg.message_text() << "\033[0m" << std::endl;
+                std::cout << fmt::format("┌[\033[32m{}\033[0m] ({}) [{}]\n└[message]> \033[33m{}\033[0m",
+                                         _msg.from(),
+                                         _msg.current_time(),
+                                         _msg.chat_name(),
+                                         _msg.message_text()) << std::endl;
             }
         }
     }
