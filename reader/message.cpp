585524b88bfe2da7ec1178a66166d234cb4ae846
LvalueReference <lvalue.github@gmail.com>
2021-07-22 20:01:31 +0300

    Update message.cpp

--- reader/message.cpp
+++ reader/message.cpp
@@ -51,8 +51,8 @@ std::string reader::message::from() {
 std::string reader::message::current_time() {
 	std::ostringstream out;
 
-	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-	std::time_t now_c = std::chrono::system_clock::to_time_t(now - std::chrono::hours(24));
+	auto now = std::chrono::system_clock::now();
+	auto now_c = std::chrono::system_clock::to_time_t(now - std::chrono::hours(24));
 
 	out << std::put_time(std::localtime(&now_c), "%T");
 	return out.str();
