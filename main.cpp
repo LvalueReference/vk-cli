01d32435f3be49e3c083e767b5243f7e1dd39022
Dian Lozinskiy <63446233+ReCpp@users.noreply.github.com>
2021-08-11 17:19:13 +0300

    Add Windows UTF-8 locale support

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
+#include <locale>
 #include "reader/reader.hpp"
 
 int main(){
-    reader::chat_reader reader;
-    reader.run();
+#ifdef _WIN32
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+#endif
+
+	reader::chat_reader reader;
+	reader.run();
 
     return EXIT_SUCCESS;
 }
