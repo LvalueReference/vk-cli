93b873ee1cf9179660273c966c3e73f537e1f42e
LvalueReference <lvalue.github@gmail.com>
2021-08-17 17:51:18 +0300

    Update network

--- lib/network/network.hpp
+++ lib/network/network.hpp
@@ -8,7 +8,7 @@
 namespace vk{
     using _param_type = std::pair<std::string, std::string>;
 
-    std::string request(std::string_view url, const std::vector<_param_type> &parameters);
+    std::string request(std::string_view url, const std::vector<_param_type>& parameters);
 }
 
 #endif //NETWORK_HPP
